fb5b76a1d80e78545df2be7250109af42c2d91f8
Rowa <jrowah@gmail.com>
2023-06-14 09:35:07 +0300

    js essentials

---
+++ scrimbajs/essential_js/cookie_concent/script.js
@@ -0,0 +1,9 @@
+console.log("What is the capital of Peru?");
+
+setTimeout(function () {
+  console.log("Lima!");
+}, 3000);
+
+setTimeout(function () {
+  console.log("Ready for next question?");
+}, 6000);
