d88eca594542d0d464da6ee13c5cc1c0f85b5360
Rowa <jrowah@gmail.com>
2023-06-14 20:07:27 +0300

    collect and log form data

--- forms/index.js
+++ forms/index.js
@@ -4,4 +4,11 @@ loginForm.addEventListener("submit", (e) => {
 
   const loginFormData = new FormData(loginForm);
   console.log(loginFormData);
+
+  const name = loginFormData.get("astronautName"); //astronautName from the name attribute
+  console.log(name);
+  const email = loginFormData.get("astronautEmail");
+  console.log(email);
+  const password = loginFormData.get("astronautPassword");
+  console.log(password);
 });
